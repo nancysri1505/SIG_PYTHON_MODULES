2463ccab01ed0f78bbcd51c30c7f22696d293940
nancysri1505 <85809274+nancysri1505@users.noreply.github.com>
2021-06-16 15:34:57 +0530

    Update 8.1.py

--- module 8/8.1.py
+++ module 8/8.1.py
@@ -1,7 +1,4 @@
-print("Nancy Srivastava 1900300109006")
-print("hello world")
-x,y=0,1
-
-while y<50:
-    print(y)
-    x,y = y,x+y
+items = [100,25,2,1,8]
+print("elements in the list are:",items)
+items_list= sum([a for a in items])
+print("sum of the elements in list are:",items_list)
